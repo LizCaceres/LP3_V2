ac953db55a24eac98b9aa5d83bafe5b367f0e959
LizCáceres <email@gmail.com>
2024-10-20 22:10:35 -0300

    Seguimiento php

---
+++ php/1.php
@@ -0,0 +1,15 @@
+<h1>Funciones con cadena</h1>
+<?php
+echo strlen ("Hola mundo desde php"); //La cantidad de valores que contiene una cadena
+echo "<hr>";
+$cadena= " Hola Mundo ";
+echo trim ($cadena);//Sacar espacios de cadenas de texto
+echo "<hr>";
+echo str_word_count ("Hola mundo desde php");//Contador de palabras
+echo "<hr>";
+$cadena = "Hola mundo desde php"
+echo strrev($cadena);//sirve para invertir valores
+echo "<hr>";
+
+
+?>
